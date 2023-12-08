7bb1c622e915df2e66eeab1af38503073619ce4b
Bondarenko Oleksandr <albom2004q@gmail.com>
2023-12-09 00:03:17 +0200

    Basics Code repo

---
+++ Basics/BasicsCodeExample/ReturnTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿async void ReturningVoidAsync(){ }
+async Task TaskVoidAsync() { }
+async Task<string> TaskStringAsync() => default; //null
+async ValueTask<string> ValueTaskStringAsync() => default; //null
+async Task<int> TaskIntAsync() => default; //0
+async ValueTask<int> ValueTaskIntAsync() => default; //0
+async Task<MyTestClass> TaskMyTestClassAsync() => new MyTestClass();
+async ValueTask<MyTestClass> ValueTaskMyTestClassAsync() => new MyTestClass();
+
+List<Delegate> AsyncFuncs =
+    new() { ReturningVoidAsync, TaskVoidAsync, TaskStringAsync, ValueTaskStringAsync, TaskIntAsync, ValueTaskIntAsync, TaskMyTestClassAsync, ValueTaskMyTestClassAsync, };
+
+foreach (var asyncFunc in AsyncFuncs) Console.WriteLine(asyncFunc.GetType());
+
+class MyTestClass { }
---
+++ Basics/BasicsCodeExample/Syntax/Program.cs
@@ -0,0 +1,12 @@
+﻿async Task HouseMaintenance(string housework, int estimatedTime)
+{
+    await Task.Run(() =>
+    {
+        for (int secondsAmount = 1; secondsAmount < estimatedTime; secondsAmount++)
+        {
+            Console.WriteLine($"Doing {housework} for {secondsAmount} seconds");
+        }
+    });
+}
+
+//This program does nothing. It is presented only to show the syntax of async/await.
