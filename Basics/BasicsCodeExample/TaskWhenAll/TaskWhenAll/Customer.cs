7849fd9d13df7f385c47345a4270004050a5107e
Bondarenko Oleksandr <albom2004q@gmail.com>
2023-12-18 19:21:00 +0200

    Customer Model

---
+++ Basics/BasicsCodeExample/TaskWhenAll/TaskWhenAll/Customer.cs
@@ -0,0 +1,11 @@
+namespace TaskWhenAll;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string first_Name { get; set; }
+    public string last_Name { get; set; }
+    public string Email { get; set; }
+
+    public override string ToString() => $"Id: {Id}; First name: {first_Name}, Last Name: {last_Name}, Email: {Email}";
+}
