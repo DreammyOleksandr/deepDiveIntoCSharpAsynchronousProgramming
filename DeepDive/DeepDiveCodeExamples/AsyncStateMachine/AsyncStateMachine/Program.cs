ed87ac19c9e4ffed2383b2b189d07702b73c67ae
Bondarenko Oleksandr <albom2004q@gmail.com>
2023-12-31 15:07:26 +0200

    Setting up the AsyncStateMachine project

---
+++ DeepDive/DeepDiveCodeExamples/AsyncStateMachine/AsyncStateMachine/Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+string jsonPathLocal = @"/Users/bondarenkooleksandr/LocalDatabase.json";
+var ParsedJSON = await GetParseLocalJSON(jsonPathLocal);
+
+Console.WriteLine(ParsedJSON.ToString());
+
+async Task<object> GetParseLocalJSON(string _jsonPath)
+{
+    string jsonContent = await File.ReadAllTextAsync(_jsonPath);
+    var records = JsonConvert.DeserializeObject(jsonContent);
+    
+    return records;
+}
+
