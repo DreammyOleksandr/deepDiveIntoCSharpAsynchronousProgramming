2ab2a52822662b5a7d2f99c62df0bcbbee5c3c3c
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-01-01 23:31:49 +0200

    Refactoring the decompiled code to readable state

--- DeepDive/DeepDiveCodeExamples/AsyncStateMachine/ServiceDecompiled/ServiceDecompiled.cs
+++ DeepDive/DeepDiveCodeExamples/AsyncStateMachine/ServiceDecompiled/ServiceDecompiled.cs
@@ -1,7 +1,7 @@
 ﻿// Decompiled with JetBrains decompiler
 // Type: AsyncStateMachine.Services.Service
 // Assembly: AsyncStateMachine, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 56636470-8303-4478-B495-BD7CD016B54A
+// MVID: E52D0AEB-E88B-46E5-B73E-4950494606E3
 // Assembly location: /Users/bondarenkooleksandr/C#Projects/DeepDiveIntoСSharpAsynchroniusProgramming/DeepDive/DeepDiveCodeExamples/AsyncStateMachine/AsyncStateMachine/bin/Debug/net7.0/AsyncStateMachine.dll
 // Local variable names from /users/bondarenkooleksandr/c#projects/deepdiveintoсsharpasynchroniusprogramming/deepdive/deepdivecodeexamples/asyncstatemachine/asyncstatemachine/bin/debug/net7.0/asyncstatemachine.pdb
 // Compiler-generated code is shown
@@ -11,13 +11,12 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncStateMachine.Services
 {
-  [NullableContext(1)]
-  [Nullable(0)]
   public class Service : 
   IService
   {
@@ -25,7 +24,6 @@ namespace AsyncStateMachine.Services
 
     public Service(string jsonPath)
     {
-      base..ctor();
       this._jsonPath = jsonPath;
     }
 
@@ -34,41 +32,33 @@ namespace AsyncStateMachine.Services
       return JsonConvert.DeserializeObject(File.ReadAllText(this._jsonPath));
     }
 
-    [AsyncStateMachine(typeof (Service.<GetParseLocalJSONAsync>d__3))]
+    [AsyncStateMachine(typeof (GetParseJSONAsyncStruct))]
     [DebuggerStepThrough]
     public Task<object> GetParseLocalJSONAsync()
     {
-      Service.<GetParseLocalJSONAsync>d__3 stateMachine = new Service.<GetParseLocalJSONAsync>d__3();
-      stateMachine.<>t__builder = AsyncTaskMethodBuilder<object>.Create();
-      stateMachine.<>4__this = this;
-      stateMachine.<>1__state = -1;
-      stateMachine.<>t__builder.Start<Service.<GetParseLocalJSONAsync>d__3>(ref stateMachine);
-      return stateMachine.<>t__builder.Task;
+      GetParseJSONAsyncStruct stateMachine = new();
+      stateMachine.builder = AsyncTaskMethodBuilder<object>.Create();
+      stateMachine.state = -1;
+      stateMachine.service = this;
+      stateMachine.builder.Start(ref stateMachine);
+      return stateMachine.builder.Task;
     }
-
+    
     [CompilerGenerated]
-    private sealed class <GetParseLocalJSONAsync>d__3 : 
-    /*[Nullable(0)]*/
-    IAsyncStateMachine
-    {
-      public int <>1__state;
-      [Nullable(0)]
-      public AsyncTaskMethodBuilder<object> <>t__builder;
-      [Nullable(0)]
-      public Service <>4__this;
-      [Nullable(0)]
-      private string <>s__1;
-      [Nullable(new byte[] {0, 1})]
-      private TaskAwaiter<string> <>u__1;
+    [StructLayout(LayoutKind.Sequential)]
+    private struct GetParseJSONAsyncStruct : 
 
-      public <GetParseLocalJSONAsync>d__3()
-      {
-        base..ctor();
-      }
+      IAsyncStateMachine
+    {
+      public int state;
+      public AsyncTaskMethodBuilder<object> builder;
+      public Service service;
+      private string receivedObject;
+      private TaskAwaiter<string> awaiter;
 
       void IAsyncStateMachine.MoveNext()
       {
-        int num1 = this.<>1__state;
+        int num1 = state;
         object result;
         try
         {
@@ -76,33 +66,33 @@ namespace AsyncStateMachine.Services
           int num2;
           if (num1 != 0)
           {
-            awaiter = File.ReadAllTextAsync(this.<>4__this._jsonPath, new CancellationToken()).GetAwaiter();
+            awaiter = File.ReadAllTextAsync(service._jsonPath, new CancellationToken()).GetAwaiter();
             if (!awaiter.IsCompleted)
             {
-              this.<>1__state = num2 = 0;
-              this.<>u__1 = awaiter;
-              Service.<GetParseLocalJSONAsync>d__3 stateMachine = this;
-              this.<>t__builder.AwaitUnsafeOnCompleted<TaskAwaiter<string>, Service.<GetParseLocalJSONAsync>d__3>(ref awaiter, ref stateMachine);
+              state = num2 = 0;
+              this.awaiter = awaiter;
+              GetParseJSONAsyncStruct stateMachine = this;
+              builder.AwaitUnsafeOnCompleted<TaskAwaiter<string>, GetParseJSONAsyncStruct>(ref awaiter, ref stateMachine);
               return;
             }
           }
           else
           {
-            awaiter = this.<>u__1;
-            this.<>u__1 = new TaskAwaiter<string>();
-            this.<>1__state = num2 = -1;
+            awaiter = this.awaiter;
+            this.awaiter = new TaskAwaiter<string>();
+            state = num2 = -1;
           }
-          this.<>s__1 = awaiter.GetResult();
-          result = JsonConvert.DeserializeObject(this.<>s__1);
+          receivedObject = awaiter.GetResult();
+          result = JsonConvert.DeserializeObject(receivedObject);
         }
         catch (Exception ex)
         {
-          this.<>1__state = -2;
-          this.<>t__builder.SetException(ex);
+          state = -2;
+          builder.SetException(ex);
           return;
         }
-        this.<>1__state = -2;
-        this.<>t__builder.SetResult(result);
+        state = -2;
+        builder.SetResult(result);
       }
 
       [DebuggerHidden]
