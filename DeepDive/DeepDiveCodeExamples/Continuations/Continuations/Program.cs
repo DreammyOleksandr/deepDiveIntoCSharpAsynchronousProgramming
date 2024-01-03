6b0e04a4edf95af174d78b76be13e0e28e5c2475
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-01-03 20:30:16 +0200

    Continuations project

---
+++ DeepDive/DeepDiveCodeExamples/Continuations/Continuations/Program.cs
@@ -0,0 +1,26 @@
+﻿using Continuations;
+using Continuations.Services;
+
+string firstLocalDbPath = @"/Users/bondarenkooleksandr/LocalDatabase.json";
+string secondLocalDbPath = @"/Users/bondarenkooleksandr/SecondDataBase.json";
+
+Service<Customer> service = new();
+
+service.GetParseLocalJSON(firstLocalDbPath)
+    .ContinueWith(customers => customers.Result.Where(x => x.Email.Contains("ol")))
+    .ContinueWith(sortedCustomers =>
+    {
+        IEnumerable<Customer> customers = sortedCustomers.Result;
+        foreach (var customer in customers)
+            Console.WriteLine(customer.ToString());
+    });
+
+await service.GetParseLocalJSON(secondLocalDbPath)
+    .ContinueWith(customers => customers.Result.Where(x => x.Email.Contains("ol")))
+    .ContinueWith(sortedCustomers =>
+    {
+        IEnumerable<Customer> customers = sortedCustomers.Result;
+        foreach (var customer in customers)
+            Console.WriteLine(customer.ToString());
+    });
+    
---
+++ DeepDive/Docs/Continuations.md
@@ -0,0 +1,6 @@
+# Continuations | [Code]()
+
+- [Continuations | Code](#continuations--code)
+  - [Theory and definitions](#theory-and-definitions)
+
+## Theory and definitions
